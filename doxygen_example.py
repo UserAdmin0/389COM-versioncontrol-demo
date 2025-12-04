aceb1f0f60bfe780b65fc1409e08a9d216a28319
UserAdmin0 <yagyansh0711@gmail.com>
2025-12-05 01:27:24 +0530

    Add Python file with Doxygen-style comments

---
+++ doxygen_example.py
@@ -0,0 +1,21 @@
+##
+# @file doxygen_example.py
+# @brief Example Python module for Doxygen documentation.
+# @author Your Name
+# @date 2025-01-01
+
+##
+# @brief Adds two numbers.
+# @param a First number.
+# @param b Second number.
+# @return The sum of a and b.
+def add(a, b):
+    return a + b
+
+##
+# @brief Multiplies two numbers.
+# @param a First number.
+# @param b Second number.
+# @return The product of a and b.
+def multiply(a, b):
+    return a * b
